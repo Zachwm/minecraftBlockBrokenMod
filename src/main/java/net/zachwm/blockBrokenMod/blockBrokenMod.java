f6668120e052e6ba5104cd12e5927b7c04c12f02
Zach McLaughlin <zachwm@byui.edu>
2024-10-27 23:04:34 -0600

    mod file built

--- src/main/java/net/zachwm/blockBrokenMod/blockBrokenMod.java
+++ src/main/java/net/zachwm/blockBrokenMod/blockBrokenMod.java
@@ -1,54 +1,23 @@
 package net.zachwm.blockBrokenMod;
 
-import com.mojang.logging.LogUtils;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(blockBrokenMod.MOD_ID)
+@Mod.EventBusSubscriber(modid = blockBrokenMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class blockBrokenMod {
     public static final String MOD_ID = "blockbrokenmod";
-    public static final Logger LOGGER = LogUtils.getLogger();
 
-    public blockBrokenMod() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        modEventBus.addListener(this::commonSetup);
-
-        MinecraftForge.EVENT_BUS.register(this);
-        modEventBus.addListener(this::addCreative);
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event) {
-
-    }
-
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    // Listen for block-breaking events
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-
-    }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-
-        }
+    public static void onBlockBroken(BlockEvent.BreakEvent event) {
+        Player player = event.getPlayer();
+        Block brokenBlock = event.getState().getBlock();
+        player.displayClientMessage(Component.literal("You broke: " + brokenBlock.getName().getString()), true);
     }
 }
